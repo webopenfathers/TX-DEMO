de193c6c18a874610106b9aa162402eb7348e4ef
webopenfathers <1290443925@qq.com>
2023-05-14 18:25:38 +0800

    feat：好看的数字滚动特效

--- tx_demo/src/App.vue
+++ tx_demo/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <!-- <DPGD /> -->
     <!-- <YCGD /> -->
-    <HTML2CANVANS />
+    <!-- <HTML2CANVANS /> -->
+    <countTo />
   </div>
 </template>
 
 <script>
 // import DPGD from './views/01-大屏文字停顿滚动特效.vue';
 // import YCGD from './views/02-云词滚动特效.vue';
-import HTML2CANVANS from './views/06-html2canvans.vue';
+// import HTML2CANVANS from './views/06-html2canvans.vue';
+import countTo from './views/07-好看的数字滚动特效.vue';
 export default {
   name: 'App',
   components: {
-    HTML2CANVANS,
+    countTo,
+    // HTML2CANVANS,
     // YCGD,
     // DPGD,
   },
---
+++ tx_demo/src/views/07-好看的数字滚动特效.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="cardList-wrapper">
+    <count-to
+      :startVal="0"
+      :endVal="12367"
+      :duration="500"
+      style="font-size: 60px"
+    />
+  </div>
+</template>
+<script>
+import countTo from 'vue-count-to';
+export default {
+  components: {
+    countTo,
+  },
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+<style></style>
