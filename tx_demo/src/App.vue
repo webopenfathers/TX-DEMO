f0bed8da00f0013c073e30edcbc4b7f623e550e8
webopenfathers <1290443925@qq.com>
2023-05-14 10:13:34 +0800

    feat：大屏文字列表停顿滚动特效

---
+++ tx_demo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <DPGD />
+  </div>
+</template>
+
+<script>
+import DPGD from './views/01-大屏文字停顿滚动特效.vue';
+export default {
+  name: 'App',
+  components: {
+    DPGD,
+  },
+};
+</script>
+
+<style></style>
